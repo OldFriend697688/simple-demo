898d90949f778d55c5486965299f6690901c662a
OldFriend <1027373676@qq.com>
2024-10-16 18:17:02 +0800

    提交一点东西试试

--- src/com/of/entity/Car.java
+++ src/com/of/entity/Car.java
@@ -1,4 +1,42 @@
 package com.of.entity;
 
 public class Car {
+    private String name;
+    private double price;
+    private String color;
+
+    public Car() {
+    }
+
+    public Car(String name, double price, String color) {
+        this.name = name;
+        this.price = price;
+        this.color = color;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+    public String getColor() {
+        return color;
+    }
+
+    public void setColor(String color) {
+        this.color = color;
+    }
+
+    // 没有实现 toString() 方法
 }
