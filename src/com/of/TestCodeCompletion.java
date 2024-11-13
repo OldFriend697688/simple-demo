947ad2d66fd8f02ff6fd13958c4a9d8151ad5faf
OldFriend <1027373676@qq.com>
2024-11-13 19:42:10 +0800

    提交一点东西试试

--- src/com/of/TestCodeCompletion.java
+++ src/com/of/TestCodeCompletion.java
@@ -1,12 +1,13 @@
 package com.of;
 
+import java.util.Objects;
+
 /**
  * 测试禁用代码自动补全
  *
  * @author OldFriend
  */
 public class TestCodeCompletion {
-
     public Dog testInstance(Dog dog) {
         return new Dog();
     }
@@ -32,6 +33,75 @@ public class TestCodeCompletion {
         @Override
         public void play() {
             System.out.println("Dog.play");
+        }
+    }
+
+    class Cat {
+        private String name;
+        private String hobby;
+        private int age;
+
+        public Cat() {
+        }
+
+        public Cat(String name, String hobby, int age) {
+            this.name = name;
+            this.hobby = hobby;
+            this.age = age;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getHobby() {
+            return hobby;
+        }
+
+        public void setHobby(String hobby) {
+            this.hobby = hobby;
+        }
+
+        public int getAge() {
+            return age;
+        }
+
+        public void setAge(int age) {
+            this.age = age;
+        }
+
+        public int add(int a, int b) {
+            return a + b;
+        }
+
+        public int add(int a, int b, int c) {
+            return a + b + c;
+        }
+
+        public int add(int a, int b, int c, int d) {
+            return a + b + c + d;
+        }
+
+        @Override
+        public String toString() {
+            return "Cat{" + "name='" + name + '\'' + ", hobby='" + hobby + '\'' + ", age=" + age + '}';
+        }
+
+        @Override
+        public boolean equals(Object o) {
+            if (this == o) return true;
+            if (o == null || getClass() != o.getClass()) return false;
+            Cat cat = (Cat) o;
+            return age == cat.age && Objects.equals(name, cat.name) && Objects.equals(hobby, cat.hobby);
+        }
+
+        @Override
+        public int hashCode() {
+            return Objects.hash(name, hobby, age);
         }
     }
 }
